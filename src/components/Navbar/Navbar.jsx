43b2c1ea44284a22cd5be4270851d64ada6b6b93
Usama Ahmed <umerusama871@gmail.com>
2026-06-05 16:36:22 +0500

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,19 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+
 export default function Navbar({ cartCount, onCartClick, whatsappNumber }) {
   return (
     <nav className="navbar">
       <a href="#home" className="brand">
-        <a href="#home" className="brand">
-  <img
-    src="/logo.png"
-    alt="Kashmiri Fresh Juice"
-    className="brand-logo"
-  />
-</a>
+        <img
+          src="/logo.png"
+          alt="Kashmiri Fresh Juice"
+          className="brand-logo"
+        />
       </a>
+
       <div className="nav-links">
-        <a className="active" href="#home">
-          Home
-        </a>
+        <a className="active" href="#home">Home</a>
         <a href="#menu">Menu</a>
         <a href="#about">About Us</a>
         <a href="#benefits">Benefits</a>
@@ -23,15 +21,18 @@ export default function Navbar({ cartCount, onCartClick, whatsappNumber }) {
         <a href="#contact">Contact</a>
         {/* <Link to="/admin">Admin</Link> */}
       </div>
+
       <div className="nav-actions">
         <button className="cart-top" onClick={onCartClick}>
           <i className="fa-solid fa-bag-shopping"></i>
           <span>{cartCount}</span>
         </button>
+
         <a
           className="whatsapp top"
           href={`https://wa.me/${whatsappNumber}?text=Hi%20Kashmiri%20Fresh%20Juices%2C%20I%20want%20to%20order`}
           target="_blank"
+          rel="noreferrer"
         >
           <i className="fa-brands fa-whatsapp"></i> Order on WhatsApp
         </a>
